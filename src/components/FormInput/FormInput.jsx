bec58f684943ee206042c3abdd4d3b6dd2357724
Vladyslav Katerusha <vladyslav.katerusha@forforce.com>
2022-04-13 21:11:39 +0300

    add auth/login + start react form validation

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,38 +1,25 @@
 import React, {useState} from 'react';
 import styles from './FormInput.module.scss';
-import { Formik } from 'formik';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(6).max(15).required()
+})
 
 const FormInput = ({title, handleClick}) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('')
 
-    const initialValues = {
-        email,
-        pass
-    };
+    const { register, handleSubmit, formState:{ errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
 
-    const validate = (values) => {
-        let errors = {};
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-
-        if (!values.email) {
-            errors.email = "Email is required";
-        } else if (!regex.test(values.email)) {
-            errors.email = "Invalid Email";
-        }
-
-        if (!values.password) {
-            errors.password = "Password is required";
-        } else if (values.password.length < 4) {
-            errors.password = "Password too short";
-        }
-
-        return errors;
-    };
-
-    const submitForm = (values) => {
-        handleClick(values)
-    };
+    const submitForm = (data) => {
+        console.log(data);
+    }
 
     return (
         // <div>
@@ -54,74 +41,15 @@ const FormInput = ({title, handleClick}) => {
         //         {title}
         //     </button>
         // </div>
-        <Formik
-            initialValues={initialValues}
-            validate={validate}
-            onSubmit={submitForm}
-        >
-            {(formik) => {
-                const {
-                    values,
-                    handleChange,
-                    handleSubmit,
-                    errors,
-                    touched,
-                    handleBlur,
-                    isValid,
-                    dirty
-                } = formik;
-                return (
-                    <div className="container">
-                        <h1>Sign in to continue</h1>
-                        <form onSubmit={handleSubmit}>
-                            <div className="form-row">
-                                <label htmlFor="email">Email</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    value={values.email}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={
-                                        errors.email && touched.email ? "input-error" : null
-                                    }
-                                />
-                                {errors.email && touched.email && (
-                                    <span className="error">{errors.email}</span>
-                                )}
-                            </div>
-
-                            <div className="form-row">
-                                <label htmlFor="password">Password</label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    value={values.pass}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={
-                                        errors.password && touched.password ? "input-error" : null
-                                    }
-                                />
-                                {errors.password && touched.password && (
-                                    <span className="error">{errors.password}</span>
-                                )}
-                            </div>
-
-                            <button
-                                type="submit"
-                                className={!(dirty && isValid) ? "disabled-btn" : ""}
-                                disabled={!(dirty && isValid)}
-                            >
-                                {title}
-                            </button>
-                        </form>
-                    </div>
-                );
-            }}
-        </Formik>
+        <div>
+            <form onSubmit={handleSubmit(submitForm)}>
+                <input type="email" name="email" placeholder="email" {...register("email")}/>
+                <span>{errors.email?.message}</span>
+                <input type="password" name="password" placeholder="password" {...register("password")} />
+                <span>{errors.password?.message}</span>
+                <input type="submit" placeholder={title}/>
+            </form>
+        </div>
     )
 }
 
