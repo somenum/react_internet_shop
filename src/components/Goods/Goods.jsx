c26ba3d6fa126a33b07b7e51720c135e2db3e9c8
Vladyslav Katerusha <vladyslav.katerusha@forforce.com>
2022-04-14 21:31:09 +0300

    load more + some fix layout

--- src/components/Goods/Goods.jsx
+++ src/components/Goods/Goods.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useSelector} from "react-redux";
 import {selectGoods} from "../../app/slices/goodsSlice";
 import styles from './Goods.module.scss';
 import GoodItem from "../GoodItem/GoodItem";
 
 const Goods = () => {
+    const [noOfElements, setNoOFElements] = useState(8)
     const goods = useSelector(selectGoods)
+
+    const slice = goods.slice(0, noOfElements)
+
+    const loadMore = () => {
+        setNoOFElements(noOfElements + noOfElements)
+    }
 
     return (
         <div className={styles.goods}>
-            {goods.map(good => (
+            {slice.map(good => (
                 <GoodItem good={good} key={good._id}/>
             ))}
+            <button
+                className={styles.goods__button}
+                onClick={() => loadMore()}
+            >
+                Load more
+            </button>
         </div>
     )
 }
